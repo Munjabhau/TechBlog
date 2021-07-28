c6f9b64030c2b9131364b61c895270ea7a6f3f1d
Munjabhau <73641523+Munjabhau@users.noreply.github.com>
2021-07-28 19:53:02 +0530

    Update settings.py

--- Icoder/Icoder/settings.py
+++ Icoder/Icoder/settings.py
@@ -16,13 +16,6 @@ from django.contrib.messages import constants as messages
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-1$!ofeu&2+_ai+ayxc0dr8__qov6b3dd(s#9zsus-dc&ji2x)&'
-
-# SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
 ALLOWED_HOSTS = []
